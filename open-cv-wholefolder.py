c3ad83ba874f77b39159df61a35f59cb08178d40
and-rew-too <93184013+and-rew-too@users.noreply.github.com>
2021-11-06 19:16:26 -0700

    Update open-cv-wholefolder.py

--- open-cv-wholefolder.py
+++ open-cv-wholefolder.py
@@ -4,22 +4,39 @@ import numpy as np
 import pandas as pd
 import glob
 from fpdf import FPDF
+import matplotlib.pyplot as plt
+
+##variables to change frequently
+# scale percent
+# buffer
+# ydist
+# threshold
+# comment or uncomment os,remove pathname to save all these files 
+
 
 imlist = []
 pdf = FPDF()
 dfy = pd.DataFrame(np.zeros((1, 1)))
 dfy = dfy.add(30)
+filelist = []
+X = np.linspace(1,900,900)
+Y = np.linspace(1,900,900)
 
-for img in glob.glob(r"C:\Users\Andrew Hu\Dropbox\PC\Desktop\*.jpg"):
-    n = cv.imread(img)
+
+for file in glob.glob(r"C:\Users\Andrew Hu\Dropbox\PC\Desktop\*.jpg"):
+#for file in glob.glob(r"C:\Users\Andrew Hu\Dropbox\PC\Desktop\val-pre-delivery\*.jpg"):
+    n = cv.imread(file)
     imlist.append(n)
+    filelist.append(file)
+
 
 #print(imlist[1])
 for i in range(0, len(imlist)):
     img = imlist[i]
     scale_percent = 15  # enter percentage of original size
-    buffer = 7*scale_percent//10  # set to 0 for the standard cropping in opencv
-    theta = 0.05  # in degrees, tells python how far to rotate CCW
+    buffer = 13*scale_percent//10  # set to 0 for the standard cropping in opencv
+    theta = 0.5  # in degrees, tells python how far to rotate CCW
+
 
     width = int(img.shape[1] * scale_percent / 100)
     height = int(img.shape[0] * scale_percent / 100)
@@ -27,17 +44,17 @@ for i in range(0, len(imlist)):
     imgr = cv.resize(img, dim, interpolation=cv.INTER_AREA)
 
     # alpha from 1.0-3.0 #beta from 0-100
-    newimg = cv.convertScaleAbs(imgr, alpha=1.5, beta=70)
+    newimg = cv.convertScaleAbs(imgr, alpha=0.55, beta=40)
     height, width = img.shape[0], img.shape[1]
     print("width: {}, height: {}".format(width, height))
 
-    ## line 34-37 finds contours in grayscale
+    ## lines 38-42 finds contours in grayscale
     imgray = cv.cvtColor(newimg, cv.COLOR_BGR2GRAY)
     ret, thresh = cv.threshold(imgray, 127, 255, 0)
     contours, hierarchy = cv.findContours(
             thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-    ## line 41-49 iterate through contours, lengths of contour, then index the largest one (the actual module)
+    ## lines 44-52 iterate through contours, lengths of contour, and index the largest one (actual module)
     size = []
     for j in range(0, len(contours)):
         size.append(len(contours[j]))
@@ -56,31 +73,73 @@ for i in range(0, len(imlist)):
     xnew = int(x - width/2)
     ynew = int(y - height/2)
     cropped = cropped[ynew:ynew+height, xnew:xnew+width]
+    print(y, y+h, x, x+w)
+
+
+
+################################################################
+    ## below is to crop based on the intensity values at halfway up the height
+    ydist = 480
+    for j in range(0,900):
+        Y[j] = imgray[ydist][j]
+        #dimensions of imgray are 600 by 900
+    threshold = max(Y)/2
+    Valarray = np.where(Y>threshold)
+    print(Valarray)
+    Valarray = np.asarray(Valarray)
+    print(Valarray[0][10:20])
+
+    plt.figure()
+    plt.title("Grayscale Intensityu")
+    plt.xlabel("x-position")
+    plt.ylabel("Pixel Intensity (grayscale)")
+    plt.plot(Y)
+    plt.xlim([0, 900])
+    plt.show()
+
+
+    ## below is to crop based on the histogram image.. idrk how to do it tho
+    # hist = cv.calcHist([imgr], [0], None, [256], [0, 256])
+    # plt.figure()
+    # plt.title("Grayscale Histogram")
+    # plt.xlabel("Bins")
+    # plt.ylabel("# of Pixels")
+    # plt.plot(hist)
+    # plt.xlim([0, 256])
+    # plt.show()
+################################################################
+
 
     ## below visualize the contours to help with debugging
-    #cv.drawContours(newimg, contours, -1, (0,255,0), 3) #draws all contours found
-    cv.drawContours(newimg, [cnt], 0, (0, 255, 0),
-                    3)  # draws the largest contour
+    #cv.drawContours(newimg, contours, -1, (0, 255, 0), 3)  # draws all contours found
+    #cv.drawContours(newimg, [cnt], 0, (0, 255, 0), 3)  # draws the largest contour
     #cv.rectangle(imgr,(x,y),(x+w,y+h),(255,0,0),3) #draws the rectangle found
+    #cv.imshow('Working Image' + filelist[i][30:], newimg)
 
-    #cv.imshow('Working Image', newimg)
-    #cv.imshow('Output Image', imgr)
+    cv.rectangle(imgr,(x,ydist),(x+w,ydist),(255,0,0),3) #draws the rectangle plotting
+    cv.imshow('Output Image', imgr)
     #cv.imshow('CROP', cropped)
     cv.waitKey(0)
 
     ##below is lines needed to add the images to a pdf, do pdf.add_page, pdf.image, pdf.output
     pathname = r'C:\Users\Andrew Hu\Dropbox\PC\Downloads\poop' + \
-        str(h) + '.jpg'  # dummy file name that gets deleted after
+        str(i+2) + '.jpg'  # dummy file name that gets deleted after
     cv.imwrite(pathname, cropped)
-
-    dfy.loc[i+1] = h
+    pdf.set_font('Arial', 'B', 8)
     if (i % 2) == 0:
         pdf.add_page()
-        pdfy = 3*dfy.loc[i]
+        pdfy = 20
+        pdf.image(pathname, x=0, y=pdfy, w=w/2, h=h/2)
     else:
-        pdfy = 0
-    pdf.image(pathname, x=0, y=pdfy, w=w/3, h=h/3)
+        pdfy = 95
+        pdf.image(pathname, x=0, y=pdfy, w=w/2, h=h/2)
+    #pdf.image(pathname, x=0, y=pdfy, w=w/2, h=h/2)
     os.remove(pathname)  # delete file name once finished
-    pdf.set_font('Arial', 'B', 16)
-    pdf.multi_cell(0, (h)+20, 'test asdfaf')
-pdf.output("yourfile.pdf", "F")
+    pdf.multi_cell(0, h/2, 'VAL: ' + filelist[i][55:])
+
+    ## below is to add single photo per page (above is two photos per page)
+    #pdf.add_page()
+    #pdf.image(pathname, x=0, y=20, w=w/2, h=h/2)
+    #pdf.set_font('Arial', 'B', 8)
+    #pdf.multi_cell(0, (h)+50, 'test' + filelist[i][38:])
+pdf.output("yourfile1.pdf", "F")
